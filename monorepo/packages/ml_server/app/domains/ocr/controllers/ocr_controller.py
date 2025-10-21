3e9f9bb8ea195f820c2822d9ea464e2950866c7c
yb <dydqls5757@gmail.com>
2025-10-21 21:39:36 +0900

    .

--- monorepo/packages/ml_server/app/domains/ocr/controllers/ocr_controller.py
+++ monorepo/packages/ml_server/app/domains/ocr/controllers/ocr_controller.py
@@ -1,6 +1,6 @@
 # app/domains/ocr/controllers/ocr_controller.py
 from app.models.ocr_model import get_ocr_model
-from fastapi import APIRouter, Depends, Form
+from fastapi import APIRouter, Depends
 from shared.core.database import get_db
 from shared.core.logging import get_logger
 from shared.service.common_service import CommonService, get_common_service
@@ -9,22 +9,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 logger = get_logger(__name__)
 
-router = APIRouter(prefix="/ocr", tags=["OCR"])
+router = APIRouter()
 
 
-@router.get("/extract")
-async def run_ocr_image_extract(
-    image_path: str = "",
-    language: str = "korean",
-    confidence_threshold: float = 0.5,
-    use_angle_cls: bool = True,
-    common_service: CommonService = Depends(get_common_service),
-    db: AsyncSession = Depends(get_db),
-):
+@router.get("/extract2")
+async def run_ocr_image_extract():
     """image ocr"""
-    model = get_ocr_model(use_angle_cls=use_angle_cls, lang=language)
-    image_data = await common_service.load_image(image_path)
-    result = model.predict(image_data, confidence_threshold)
+    logger.info("asdasd")
+    # model = get_ocr_model(use_angle_cls=use_angle_cls, lang=language)
+    # image_data = await common_service.load_image(image_path)
+    # result = model.predict(image_data, confidence_threshold)
     # OCRExecution 생성
     # ocr_execution_data = OCRExecutionCreate(
     #     chain_id=chain_id,
@@ -50,26 +44,11 @@ async def run_ocr_image_extract(
 
     # logger.info(f"OCR 실행 정보 DB 저장 완료: ID={db_ocr_execution.id}")
     # OCRResultDTO.model_validate(db_ocr_execution)
-    return ResponseBuilder.success(data=result, message="")
+    return ResponseBuilder.success(data="", message="")
 
 
-@router.get("/languages")
-async def get_supported_languages():
-    """지원하는 언어 목록 조회"""
-    languages = [
-        {"code": "korean", "name": "한국어"},
-        {"code": "english", "name": "영어"},
-        {"code": "chinese", "name": "중국어"},
-        {"code": "japanese", "name": "일본어"},
-    ]
-
-    return ResponseBuilder.success(
-        data={"languages": languages}, message="지원 언어 목록"
-    )
-
-
-@router.get("/health")
-async def health_check():
+@router.get("/sdsd")
+async def sdsd():
     """헬스 체크"""
     return ResponseBuilder.success(
         data={"status": "healthy"}, message="OCR 서비스 정상"
